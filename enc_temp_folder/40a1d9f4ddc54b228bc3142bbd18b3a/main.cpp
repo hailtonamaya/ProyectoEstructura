4e1b1b8956a5a356152e1da075069edf0cc9d146
HAILTON RAMIRO AMAYA SCHRUNDER <hailtonamaya@unitec.edu>
2023-02-11 13:55:18 -0600

    Cout

--- enc_temp_folder/40a1d9f4ddc54b228bc3142bbd18b3a/main.cpp
+++ enc_temp_folder/40a1d9f4ddc54b228bc3142bbd18b3a/main.cpp
@@ -1,12 +1,10 @@
 #include "ProyectoEstructura.h"
 #include <QtWidgets/QApplication>
-using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     ProyectoEstructura w;
     w.show();
-    //cout << "Hola Mundo";
     return a.exec();
 }
